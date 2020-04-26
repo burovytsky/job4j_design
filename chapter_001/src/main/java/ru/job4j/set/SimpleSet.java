706ae5cf6cc20d8fe301f538835163c11482ed72
Constantine Burovytsky <esfero1505@gmail.com>
2020-04-26 11:22:22 +0300

    1. Реализовать коллекцию Set на массиве[#276283]

---
+++ chapter_001/src/main/java/ru/job4j/set/SimpleSet.java
@@ -0,0 +1,31 @@
+package ru.job4j.set;
+
+import ru.job4j.list.SimpleArray;
+
+import java.util.Iterator;
+
+public class SimpleSet<E> implements Iterable<E> {
+    private final SimpleArray<E> simpleArray = new SimpleArray<>();
+    private int size = 0;
+
+    public void add(E element) {
+        boolean rsl = true;
+        if (size != 0) {
+            for (int i = 0; i < size; i++) {
+                if (simpleArray.get(i).equals(element)) {
+                    rsl = false;
+                    break;
+                }
+            }
+        }
+        if (rsl) {
+            simpleArray.add(element);
+            size++;
+        }
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return simpleArray.iterator();
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/set/SimpleSetTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.set;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class SimpleSetTest {
+    @Test
+    public void whenAddThenIt() {
+        SimpleSet<String> array = new SimpleSet<>();
+        array.add("first");
+        Iterator<String> iterator = array.iterator();
+        assertThat(iterator.next(), is("second"));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenAddSameElementItsNotAdded() {
+        SimpleSet<String> array = new SimpleSet<>();
+        array.add("first");
+        array.add("first");
+        array.add("second");
+        Iterator<String> iterator = array.iterator();
+        assertThat(iterator.next(), is("first"));
+        assertThat(iterator.next(), is("second"));
+        iterator.next();
+    }
+
+}
