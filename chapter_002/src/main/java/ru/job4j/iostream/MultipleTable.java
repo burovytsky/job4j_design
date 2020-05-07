89afbaa8ccba8276ead30980c975f0cfabfabe7a
burovytsky <esfero1505@gmail.com>
2020-05-07 18:07:25 +0200

    0.1. FileOutputStream. [#276310]

---
+++ chapter_002/src/main/java/ru/job4j/iostream/MultipleTable.java
@@ -0,0 +1,25 @@
+package ru.job4j.iostream;
+
+import java.io.FileOutputStream;
+import java.util.Arrays;
+
+public class MultipleTable {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("multiplication table.txt")) {
+            String table = Arrays.deepToString(multiple(10));
+            out.write(table.getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int[][] multiple(int size) {
+        int[][] table = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                table[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        return table;
+    }
+}
