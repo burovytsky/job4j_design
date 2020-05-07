5ba18e385e191728119893327f36235fa1c4fa8f
Constantine Burovytsky <esfero1505@gmail.com>
2020-05-07 10:27:48 +0300

    2. Статистику по коллекции. [#276256]

---
+++ chapter_001/src/main/java/ru/job4j/analysis/Analysis.java
@@ -0,0 +1,44 @@
+package ru.job4j.analysis;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class Analysis {
+
+    public static Info diff(List<User> previous, List<User> current) {
+        Info info = new Info();
+        Map<Integer, String> map = new HashMap<>();
+        for (User user : current) {
+            map.put(user.id, user.name);
+        }
+        for (User prevUser : previous) {
+            String userValue = map.get(prevUser.id);
+            if (userValue == null) {
+                info.deleted++;
+            } else if (!userValue.equals(prevUser.name)) {
+                info.changed++;
+            }
+        }
+
+        info.added = current.size() - (previous.size() - info.deleted);
+        return info;
+    }
+
+    public static class User {
+        int id;
+        String name;
+
+        public User(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+    }
+
+    public static class Info {
+        int added;
+        int changed;
+        int deleted;
+    }
+}
