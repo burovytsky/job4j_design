a10ad33872616e8c32f0dc39112915b6f488eb84
Constantine Burovytsky <esfero1505@gmail.com>
2020-05-20 09:30:58 +0300

    0. Что такое Socket? [#276324]исправил ошибки

--- chapter_002/src/main/java/ru/job4j/socket/EchoServer.java
+++ chapter_002/src/main/java/ru/job4j/socket/EchoServer.java
@@ -18,6 +18,7 @@ public class EchoServer {
                     String str;
                     while (!(str = in.readLine()).isEmpty()) {
                         if (str.equals("GET /?msg=Bye HTTP/1.1")) {
+                            out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
                             out.flush();
                             server.close();
                             break;
@@ -31,5 +32,34 @@ public class EchoServer {
                 }
             }
         }
+//        try (ServerSocket server = new ServerSocket(9000)) {
+//            while (true) {
+//                Socket socket = server.accept();
+//                try (OutputStream out = socket.getOutputStream();
+//                     BufferedReader in = new BufferedReader(
+//                             new InputStreamReader(socket.getInputStream()))) {
+//                    String str;
+//                    while (!(str = in.readLine()).isEmpty()) {
+//                        if (str.contains("GET")) {
+//                            String msg = str.replace("GET /?msg=", "").split(" ")[0];
+//                            if (msg.equals("Exit")) {
+//                                out.flush();
+//                                server.close();
+//                                break;
+//                            } else if (msg.equals("Hello")) {
+//                                out.write("Hello, user".getBytes());
+//                            } else {
+//                                out.write(msg.getBytes());
+//                            }
+//                        }
+//                    }
+//                    if (server.isClosed()) {
+//                        break;
+//                    }
+//                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+//                }
+//            }
+//        }
+//    }
     }
 }
