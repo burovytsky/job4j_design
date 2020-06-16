cff1868eeba5a32f3f044ce098ff908edfa17bbb
burovytsky <esfero1505@gmail.com>
2020-06-16 11:21:33 +0300

    Демонстрация работы GC [#276354]

---
+++ chapter_004/src/main/java/ru/job4j/MemoryInfo.java
@@ -0,0 +1,22 @@
+package ru.job4j;
+
+public class MemoryInfo {
+    public static void main(String[] args) {
+        memInfo();
+        for (int i = 0; i < 600; i++) {
+          new User("name"+i, "s"+i);
+        }
+        memInfo();
+    }
+    private static void memInfo() {
+        Runtime runtime = Runtime.getRuntime();
+
+        System.out.println("used memory " + (runtime.totalMemory() - runtime.freeMemory()));
+
+        System.out.println("free memory " + runtime.freeMemory());
+
+        System.out.println("total memory " + runtime.totalMemory());
+
+        System.out.println("=======================================");
+    }
+}
