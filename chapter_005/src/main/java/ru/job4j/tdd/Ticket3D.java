ae50f9d1ecc6d9ee66f8d18884409a79f170b61f
Constantine Burovytsky <esfero1505@gmail.com>
2020-06-21 18:11:48 +0300

    2. Что такое TDD? [#276330] добавил заглушки

--- chapter_005/src/main/java/ru/job4j/tdd/Ticket3D.java
+++ chapter_005/src/main/java/ru/job4j/tdd/Ticket3D.java
@@ -1,4 +1,13 @@
 package ru.job4j.tdd;
 
 public class Ticket3D implements Ticket {
+    @Override
+    public int getRow() {
+        return 0;
+    }
+
+    @Override
+    public int getColumn() {
+        return 0;
+    }
 }
