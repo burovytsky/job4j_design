59c4c699c0ac0008962747e41ed9d18eefa7dcac
burovytsky <esfero1505@gmail.com>
2020-07-17 09:57:19 +0300

    удалил car_parking_old

--- chapter_005/src/main/java/ru/job4j/lsp/car_parking/PassengerCar.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j.lsp.car_parking;
-
-public class PassengerCar extends Car {
-
-    public PassengerCar(String name, String number, int size) {
-        super(name, number, size);
-    }
-}
--- chapter_005/src/main/java/ru/job4j/lsp/car_parking/Truck.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j.lsp.car_parking;
-
-public class Truck extends Car {
-
-    public Truck(String name, String number, int size) {
-        super(name, number, size);
-    }
-}
